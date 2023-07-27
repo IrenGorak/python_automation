b49755fbc4feb688b5d4331d95819da09734403f
Iren <iren.horak@gmail.com>
2023-07-27 15:09:03 +0300

    added tables_test

--- generator/generator.py
+++ generator/generator.py
@@ -1,3 +1,5 @@
+import random
+
 from data.data import Person
 from faker import Faker
 
@@ -8,6 +10,11 @@ Faker.seed()
 def generated_person():
     yield Person(
         full_name=faker_eng.first_name() + " " + faker_eng.last_name(),
+        first_name=faker_eng.first_name(),
+        last_name=faker_eng.first_name(),
+        age=random.randint(18, 80),
+        department=faker_eng.job(),
+        salary=random.randint(1500, 3000),
         email=faker_eng.email(),
         current_address=faker_eng.street_address(),
         permanent_address=faker_eng.street_name(),
--- pages/elemets_page.py
+++ pages/elemets_page.py
@@ -3,7 +3,8 @@ import random
 from selenium.webdriver.common.by import By
 
 from generator.generator import generated_person
-from locators.elements_page_locator import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonLocators
+from locators.elements_page_locator import TextBoxPageLocators, CheckBoxPageLocators, RadioButtonLocators, \
+    WebTablePageLocators
 from pages.base_page import BasePage
 
 
@@ -78,3 +79,42 @@ class RadioButtonPage(BasePage):
     def get_output_result(self):
         return self.element_is_present(self.locators.OUTPUT_RESULT).text
 
+
+class WebTablePage(BasePage):
+    locators = WebTablePageLocators()
+
+    def add_new_person(self):
+        count = 1
+        while count != 0:
+            person_info = next(generated_person())
+            first_name = person_info.first_name
+            last_name = person_info.last_name
+            email = person_info.email
+            age = person_info.age
+            salary = person_info.salary
+            department = person_info.department
+            self.element_is_visible(self.locators.ADD_NEW_PERSON_BUTTON).click()
+            self.element_is_visible(self.locators.FIRST_NAME).send_keys(first_name)
+            self.element_is_visible(self.locators.LAST_NAME).send_keys(last_name)
+            self.element_is_visible(self.locators.USER_EMAIL).send_keys(email)
+            self.element_is_visible(self.locators.USER_AGE).send_keys(age)
+            self.element_is_visible(self.locators.USER_SALARY).send_keys(salary)
+            self.element_is_visible(self.locators.USER_DEPARTMENT).send_keys(department)
+            self.element_is_visible(self.locators.SUBMIT_BUTTON).click()
+            count -= 1
+            return [first_name, last_name, str(age), email, str(salary), department]
+
+    def check_new_added_person(self):
+        person_list = self.elements_are_present(self.locators.PERSON_LIST)
+        data = []
+        for item in person_list:
+            data.append(item.text.splitlines())
+        return data
+
+    def search_person_in_table(self, key_word):
+        self.element_is_visible(self.locators.SEARCH_FIELD).send_keys(key_word)
+
+    def check_the_search_result(self):
+        delete_button = self.element_is_present(self.locators.DELETE_BUTTON)
+        row = delete_button.find_element(By.XPATH, "//*[@id='app']/div/div/div[2]/div[2]/div[2]/div[3]/div[1]/div[2]")
+        return row.text.splitlines()
