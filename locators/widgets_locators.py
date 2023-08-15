6cc35f450bab41f2885f6cd31cc5911badc71145
Iren <iren.horak@gmail.com>
2023-08-15 14:28:06 +0300

    add auto_compete test

--- locators/widgets_locators.py
+++ locators/widgets_locators.py
@@ -8,3 +8,11 @@ class AccordianPageLocators:
     CONTENT_SECTION_2 = (By.ID, "section2Content")
     SECTION_3 = (By.ID, "section3Heading")
     CONTENT_SECTION_3 = (By.ID, "section3Content")
+
+
+class AutoCompletePageLocators:
+    MULTIPLE_INPUT = (By.CSS_SELECTOR, 'input[id="autoCompleteMultipleInput"]')
+    MULTIPLE_OUTPUT = (By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"]')
+    REMOVE_CHOSEN_INPUT = (By.CSS_SELECTOR, 'div[class="css-xb97g8 auto-complete__multi-value__remove"]')
+    SINGLE_FIELD = (By.CSS_SELECTOR, "div[class='auto-complete__single-value css-1uccc91-singleValue']")
+    SINGLE_INPUT = (By.CSS_SELECTOR, "input[id='autoCompleteSingleInput']")
